655e7c0c7e6e84a27bbef25839b143dbd39236a5
Guillaume Balas <guillaume.balas@gmail.com>
2018-11-07 17:08:26 -0600

    Seed file adjusted v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,10 @@
 puts 'Cleaning database...'
 Book.destroy_all
 AmazonReview.destroy_all
+Tweets.destroy_all
 
 puts 'Creating books...'
 NYTService.new.seed_db
-
-puts 'Cleaning reviews...'
-AmazonReview.destroy_all
 
 puts 'Searching & assigning Amazon reviews to books...'
 AmazonBookScrapingService.new.seed_db
