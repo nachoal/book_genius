f666b9657b17965fab5e801ca700ae48d7dbacfa
Guillaume Balas <guillaume.balas@gmail.com>
2018-11-07 17:05:58 -0600

    Seed file comments removed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# puts 'Cleaning database...'
-# Book.destroy_all
-# AmazonReview.destroy_all
+puts 'Cleaning database...'
+Book.destroy_all
+AmazonReview.destroy_all
 
-# puts 'Creating books...'
-# NYTService.new.seed_db
+puts 'Creating books...'
+NYTService.new.seed_db
 
 puts 'Cleaning reviews...'
 AmazonReview.destroy_all
@@ -11,10 +11,10 @@ AmazonReview.destroy_all
 puts 'Searching & assigning Amazon reviews to books...'
 AmazonBookScrapingService.new.seed_db
 
-# puts 'Searching & assigning tweets to books...'
-# TwitterService.tweet_text_save
+puts 'Searching & assigning tweets to books...'
+TwitterService.tweet_text_save
 
-# puts 'Calling Aylien API to get sentiments from twitter and assigning result to book.aylien_result...'
-# AylienService.book_sentiment_save
+puts 'Calling Aylien API to get sentiments from twitter and assigning result to book.aylien_result...'
+AylienService.book_sentiment_save
 
 puts 'Finished!'
