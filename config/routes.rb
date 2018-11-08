26eacdfa340a3bd5a5a6af6a2271ed8f8b0610e4
Ignacio Alonso <nacho.allibre@gmail.com>
2018-11-08 15:57:31 -0600

    changed routes to go straight to the book list that we currently have

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
+  root to: 'books#index'
   devise_for :users
-  root to: 'pages#home'
-  resources :books 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
   resources :static_pages
   get '/about', to: 'static_pages#about'
   get '/collection', to: 'static_pages#collection'
