3613a3863c0acfd9207b50c72016e58de6bb14f0
Guillaume Balas <guillaume.balas@gmail.com>
2018-11-15 11:00:59 -0600

    Home page design of search bar refined

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-puts 'Cleaning database...'
-Book.destroy_all
+# puts 'Cleaning database...'
+# Book.destroy_all
 
-puts 'Creating books...'
-NYTService.new.seed_db
+# puts 'Creating books...'
+# NYTService.new.seed_db
 
-puts 'Searching & assigning tweets to books...'
-TwitterService.tweet_save
+# puts 'Searching & assigning tweets to books...'
+# TwitterService.tweet_save
 
-puts 'Getting Aylien sentiment for each individual tweet'
-AylienService.individual_tweet_sentiment
+# puts 'Getting Aylien sentiment for each individual tweet'
+# AylienService.individual_tweet_sentiment
 
 puts 'Searching & assigning Amazon reviews to books...'
 AmazonBookScrapingService.new.seed_db(9) # Parameter: max number of review pages to scrape"
